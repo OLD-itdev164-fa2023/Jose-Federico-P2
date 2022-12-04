45284029e9788c0cfd3531fd6a023eb3d4e87440
Jose Federico <federicj@gmatc.matc.edu>
2022-12-04 01:33:59 -0600

    Completed P2

---
+++ gatsby-gallery-project/src/components/HeroSection/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeroSection.css"
+
+function HeroSection(){
+    return(
+        <div className="hero-container">
+            <h1>Project 2</h1>
+            <p>Gatbsy Gallery by Jose Federico</p>
+            <p>*Scroll Down*</p>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ gatsby-gallery-project/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HeroSection from "../components/HeroSection"
+import Image from "../components/image"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home"/>
+    <HeroSection/>
+    <Image/>
+  </Layout>
+)
+
+export default IndexPage
